46cbfc8308119fc702d550eb927d9729722bf1e0
karaefendii <116747978+karaefendii@users.noreply.github.com>
2024-12-11 01:23:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ydF1VCpQ#DGVOUk3F1HvTb0Gk1dYRkJWQhvI_eFpMlbYnqJdEIdE",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
-SUDO_NB: process.env.SUDO_NB || "94742398003",
+ALIVE_MSG: process.env.ALIVE_IMG || "*Hoşgeldin Dostum* ${pushname}\n *İyi Eğlenceler Dilerim*",
+SUDO_NB: process.env.SUDO_NB || "905541878489",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 AUTO_VOICE:"true"
 };
 
